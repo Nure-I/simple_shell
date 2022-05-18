eba4f123033c0ab98bfc3cc04f73703d74b4173d
Nure-I <ibrahimsaladin1@gmail.com>
2022-05-18 14:42:19 +0300

    shell

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,16 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * _puts - prints a string
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+void _puts(char *str)
 {
-	return (write(1, &c, 1));
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
 }
